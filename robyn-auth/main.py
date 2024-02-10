a1213ad93c2ef5eda9a05f5e11509416756daaa1
rulanugrh <nextarituenak01@gmail.com>
2024-02-10 14:57:25 +0700

    feat: handling exception

--- robyn-auth/main.py
+++ robyn-auth/main.py
@@ -5,6 +5,10 @@ from config import config
 conf = config.GetConfig()
 app = Robyn(__file__)
 
+@app.exception
+def handle_exception(error):
+    return {"status_code": 500, "msg": f"something error: {error}"}
+    
 app.include_router(user.user_router)
 app.include_router(todo.todo_router)
 
